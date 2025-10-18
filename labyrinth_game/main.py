7065a8685d1bac52d5400c664f4dcc03c1fd39ac
Victoria Zhabinskaya <zhabinskayavictoria@yandex.ru>
2025-10-18 15:58:45 +0300

    Add basic data

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+
+from labyrinth_game.constants import ROOMS
+from labyrinth_game.player_actions import *
+from labyrinth_game.utils import *
+
+game_state = {
+    'player_inventory': [],
+    'current_room': 'entrance',
+    'game_over': False,
+    'steps_taken': 0
+}
+
 def main():
-    print("Первая попытка запустить проект!")
+    print("Привет!")
 
 if __name__ == "__main__":
     main()
