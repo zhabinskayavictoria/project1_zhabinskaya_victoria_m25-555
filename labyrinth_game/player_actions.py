f7ccde7e066d5be4a57166cdebb3f9202c35e528
Victoria Zhabinskaya <zhabinskayavictoria@yandex.ru>
2025-10-18 23:12:53 +0300

    Add movement function

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -1,3 +1,6 @@
+from labyrinth_game.constants import ROOMS
+from labyrinth_game.utils import describe_current_room, show_inventory
+
 def show_inventory(game_state):
     inventory = game_state['player_inventory']
 
@@ -15,3 +18,14 @@ def get_input(prompt="> "):
         print("\nВыход из игры.")
         return "quit"
 
+
+def move_player(game_state, direction):
+    current_room = game_state['current_room']
+    room_data = ROOMS[current_room]
+    exits = room_data.get('exits', {})
+    if direction in exits:
+        game_state['current_room'] = exits[direction]
+        game_state['steps_taken'] += 1
+        describe_current_room(game_state)
+    else:
+        print("Нельзя пойти в этом направлении.")
