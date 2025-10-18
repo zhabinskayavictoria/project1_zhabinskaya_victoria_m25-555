6982cba92f3552b2579cfcad67b65b7d47338f83
Victoria Zhabinskaya <zhabinskayavictoria@yandex.ru>
2025-10-18 23:21:27 +0300

    Add taking item function

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -29,3 +29,14 @@ def move_player(game_state, direction):
         describe_current_room(game_state)
     else:
         print("Нельзя пойти в этом направлении.")
+        
+def take_item(game_state, item_name):
+    current_room = game_state['current_room']
+    room_items = ROOMS[current_room].get('items', [])
+    if item_name in room_items:
+        game_state['player_inventory'].append(item_name)
+        room_items.remove(item_name)
+        print(f"Вы подняли: {item_name}")
+    else:
+        print("Такого предмета здесь нет.")
+        
